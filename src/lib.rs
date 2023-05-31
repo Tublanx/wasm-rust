5abcd6904250403c630f6f4b615fead0074f7ade
Tublanx <rlgus03453@naver.com>
2023-05-31 22:24:06 +0900

    initialize project

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+struct Hello {}
+
+impl Component for Hello {
+    type Message = ();
+    type Properties = ();
+
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        true
+    }
+
+    fn view(&self) -> Html {
+        html! { <span>{"Hello World"}</span> }
+    }
+}
+
+#[wasm_bindgen]
+pub fn run_app() {
+    App::<Hello>::new().mount_to_body();
+}
